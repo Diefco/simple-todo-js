163941bbf4bd402339413b16c38cf3235b0b1c1b
Diefco <diefco.web@gmail.com>
2022-10-15 22:46:49 -0500

    ToDo APP - First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# simple-todo-js
+
+ToDo APP creada con el curso de Javascript Moderno de Fernando Herrera.
+
+### Notes:
+
+Remember to rebuild the node modules.
+
+```
+npm install
+```
+
+And to build the production build:
+
+```
+npm run build
+```
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './css/styles.css';
+
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+export const todoList = new TodoList();
+
+console.log(todoList.todos);
+
+todoList.todos.forEach(crearTodoHtml);
+
+console.log('todos', todoList.todos);
